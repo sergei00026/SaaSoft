63e325545b98cf73ad1a76e73d6a9ba6c4c2315a
sergei00026 <sergei57fx@mail.ru>
2026-02-04 18:57:55 +0300

    feat: add dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { ghPages } from 'vite-plugin-gh-pages'
+import path from 'node:path'
 
 export default defineConfig({
-  base: '/SaaSoft/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-    ghPages({
-      branch: 'gh-pages',
-      repo: 'https://github.com/sergei00026/SaaSoft.git',
-      dotfiles: true
-    })
-  ],
+  base: './',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-
 })
