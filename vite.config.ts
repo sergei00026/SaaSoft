ed166049a48d3b8192b96b4ff14cf3fe4ae80d07
sergei00026 <sergei57fx@mail.ru>
2026-02-04 17:57:38 +0300

    feat: add GH_TOKEN

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { ghPages } from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/SaaSoft/',
   plugins: [
     vue(),
     vueDevTools(),
+    ghPages({
+      branch: 'gh-pages',
+      repo: 'https://github.com/sergei00026/SaaSoft.git',
+      dotfiles: true
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
 })
